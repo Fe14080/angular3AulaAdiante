fa50cb6375416511040df9870aedc560deab39e5
Fe14080 <lipe.cintran1408@gmail.com>
2022-04-20 21:02:21 -0300

    1 file modified, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { CardComponent } from './card/card.component';
 import { ButaoComponent } from './butao/butao.component';
 import { AlertaComponent } from './alerta/alerta.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, CardComponent, ButaoComponent, AlertaComponent],
+  declarations: [AppComponent, ButaoComponent, AlertaComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
