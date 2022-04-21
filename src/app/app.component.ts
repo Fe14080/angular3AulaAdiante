28d6813e3e496ab9486da3940b61c304b28e43d0
Fe14080 <lipe.cintran1408@gmail.com>
2022-04-21 20:56:26 -0300

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,39 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   contador = 0;
+
+  users: any[];
+
+  page: number = 1;
 
   onButtonClick() {
     this.contador++;
     console.log(this.contador);
   }
+
+  ngOnInit(): void {
+    this.UserService.getUser(this.page).subscribe((objResult) => {
+      this.users = objResult.results;
+    });
+  }
+
+  pageProx(): void {
+    this.page++;
+    this.ngOnInit();
+  }
+
+  pageAnt(): void {
+    this.page--;
+    this.ngOnInit();
+  }
+
+  constructor(public UserService: UserService) {}
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,7 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
-@Injectable()
+@Injectable({ providedIn: 'root' })
 export class UserService {
   constructor(public httpClient: HttpClient) {}
+
+  getUser(page: number = 1, quantidade: number = 5): Observable<any> {
+    return this.httpClient.get(
+      `https://randomuser.me/api/?page=${page}&results=${quantidade}&seed=abc`
+    );
+  }
 }
